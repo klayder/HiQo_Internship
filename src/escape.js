c11e82bfb272f655f79c0d9564d787f277586801
Andrey Sorokin <anderysorokin@gmail.com>
2018-06-23 20:56:35 +0300

    add methods to check and make steps

--- src/escape.js
+++ src/escape.js
@@ -2,7 +2,110 @@
 class Escape {
     constructor(labyrinth) {
 		this.labyrinth=labyrinth;
+		this.startPosition=[0,1];
+		this.currentPosition=this.startPosition;
+		this.breadСrumbs=[];
+		this.availableWays;
+		this.labyrinthMap={};
+		this.nextMove=null;
 	}
+
+	checkPass(posX,posY){
+		if(posX<0||posY<0) 
+			return false;
+		return this.labyrinth[posY][posX]===0?true:false;
+	}
+
+	checkWays(){
+		this.availableWays= {
+				"top": this.checkPass(this.currentPosition[1],this.currentPosition[0]-1),
+				"bottom": this.checkPass(this.currentPosition[1],this.currentPosition[0]+1),
+				"left": this.checkPass(this.currentPosition[1]-1,this.currentPosition[0]),
+				"right": this.checkPass(this.currentPosition[1]+1,this.currentPosition[0])
+		};
+		return this
+	}
+
+
+	checkMap(){
+		let positionIndex=this.currentPosition.join(":");
+		if(this.labyrinthMap[positionIndex]){
+			this.availableWays=this.labyrinthMap[positionIndex];
+		} else {
+			this.checkWays();
+			let prevPosition=this.breadСrumbs[0];
+			if(prevPosition){
+				this.availableWays[prevPosition.from]='back';
+			}
+		}
+		return this
+	}
+
+	choseDirection(){
+		for(let dir in this.availableWays){
+			if(this.availableWays[dir]===true){
+				this.nextMove=dir;
+			}
+		} 
+
+		if(!this.nextMove){
+			this.nextMove='back';
+		}
+
+		return this
+	}
+
+	makeMove(){
+		let oppositeSide={
+				'top'	:	'bottom',
+				'bottom':	'top',
+				'left'	: 	'right',
+				'right'	: 	'left'
+			},
+			positionIndex=this.currentPosition.join(":"),
+			breadСrumb={
+				'from'	: 	oppositeSide[this.nextMove],
+				'to'	: 	this.nextMove,
+				'position' : 	positionIndex
+			};
+
+			this.breadСrumbs.unshift(breadСrumb);
+			this.labyrinthMap[positionIndex]=this.availableWays;
+
+			switch(this.nextMove) {
+			  case 'top':  
+			  	this.currentPosition[0]-=1;
+			    break;
+			  case 'bottom':  
+			  	this.currentPosition[0]+=1;
+			    break;
+			  case 'left':  
+			  	this.currentPosition[1]-=1;
+			    break;
+			  case 'right':  
+			  	this.currentPosition[1]+=1;
+			    break;
+
+			  default:
+			    
+			    break
+			}
+			
+
+		return this;
+	}
+
+
+	findeWayOut(){
+		while(this.nextMove!='back'){
+			this.nextMove=null;
+			this.checkMap().choseDirection().makeMove();
+		}
+		
+	}
+	/*Need add method to backSteps if i go to the dead end*/
+
+	
 }
 
 module.exports = Escape;
--- test/labyrinth.spec.js
+++ test/labyrinth.spec.js
@@ -6,11 +6,73 @@ const labyrinth=require('../src/labyrinth.json');
 describe('Escape', () => {
     describe('Enter point', () => {
         const escape = new Escape(labyrinth.mainLabyrinth);
-
+        
         it('Enter point must be on position x=1; y=0', () => {
             expect(escape.labyrinth[0][1]).to.equal(0);
         })
     });
+
+    describe('Check Pass', () => {
+        const escape = new Escape(labyrinth.mainLabyrinth);
+        	
+        it('Must return true on position 1,0', () => {
+            expect(escape.checkPass(1,0)).to.equal(true);
+        })
+        it('Must return false on position 0,0', () => {
+            expect(escape.checkPass(0,0)).to.equal(false);
+        })
+        it('Must return true on position 1,1', () => {
+            expect(escape.checkPass(1,1)).to.equal(true);
+        })
+        it('Must return false on position 2,2', () => {
+            expect(escape.checkPass(2,2)).to.equal(false);
+        })
+       
+    });
+
+    describe('Check ways', () => {
+        const escape = new Escape(labyrinth.mainLabyrinth);
+        	escape.checkWays();
+        it(`Can i go UP from position ${escape.currentPosition}
+        	Must return 'false'`, () => {
+            expect(escape.availableWays.top).to.equal(false);
+        })
+        it(`Can i go BOTTOM from position ${escape.currentPosition}
+        	Must return 'true'`, () => {
+            expect(escape.availableWays.bottom).to.equal(true);
+        })
+        it(`Can i go left from position ${escape.currentPosition}
+        	Must return 'false'`, () => {
+            expect(escape.availableWays.left).to.equal(false);
+        })
+        it(`Can i go right from position ${escape.currentPosition}
+        	Must return 'false'`, () => {
+            expect(escape.availableWays.right).to.equal(false);
+        })
+    });
+
+    describe('Chose Dir', () => {
+        const escape = new Escape(labyrinth.mainLabyrinth);
+
+        	console.log(escape.currentPosition);
+        	escape.currentPosition=[13,27];
+        	escape.checkMap().choseDirection();
+        	//escape.findeWayOut();
+        	console.log(escape.breadСrumbs);
+        	console.dir(escape.availableWays);
+        	console.log(escape.currentPosition);
+        	
+       
+        it('Must be bottom', () => {
+            expect(escape.nextMove).to.equal('right');
+        })
+       
+        
+        
+        
+    });
+
+
 
     
 });
